7dad0e0a57ec3a9112a08d3b42b4ad5d2b9831f7
Casca113s2 <cascabusiness@gmail.com>
2024-07-26 12:36:49 +0700

    Dev: Fix install-jenkins.sh with service file

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -7,12 +7,13 @@ JAVA_BASE_DIR="/opt/jdk"
 JENKINS_VERSION="2.452.3"
 JENKINS_WAR_URL="https://updates.jenkins.io/download/war/${JENKINS_VERSION}/jenkins.war"
 JENKINS_BASE_DIR="/opt/jenkins"
+JENKINS_HOME_BASE="/jenkins"
 
 ### Enable debugging ###
 set -x
 
 ### Install Frontconfig ###
-sudo apt install fontconfig
+sudo apt install -y fontconfig
 
 ### Install Java ###
 # Download jdk .tar.gz file
@@ -33,28 +34,52 @@ sudo update-alternatives --install /bin/java java /opt/jdk/${JAVA_JDK_VERSION}/b
 sudo update-alternatives --install /bin/javac javac /opt/jdk/${JAVA_JDK_VERSION}/bin/javac 100
 
 ### Install Jenkins ###
+sudo mkdir -p ${JENKINS_WAR_DIR}
+
+# Download Jenkins WAR file if it doesn't already exist
+if [ ! -f ${JENKINS_WAR_DIR}/jenkins.war ]; then
+    sudo wget ${JENKINS_WAR_URL} -O ${JENKINS_WAR_DIR}/jenkins.war
+fi
+
 # Function to set up Jenkins instance
 setup_jenkins_instance() {
   local port=$1
-  local instance_dir="${BASE_DIR}/jenkins_${port}"
+  local instance_dir="${JENKINS_HOME_BASE}/jenkins_${port}"
 
   # Create the instance directory
   sudo mkdir -p ${instance_dir}
-
-  # Download Jenkins WAR file if it doesn't already exist
-  if [ ! -f ${instance_dir}/jenkins.war ]; then
-  	sudo wget -O ${instance_dir}/jenkins.war ${JENKINS_WAR_URL}
-  fi
 
   # Change permissions to ensure the Jenkins user can access it
   sudo chown -R $(whoami):$(whoami) ${instance_dir}
   sudo chmod -R 755 ${instance_dir}
 
-  # Start Jenkins instance on the specified port with a context path
-  nohup java -jar ${instance_dir}/jenkins.war --httpPort=${port} --prefix="/jenkins" &> ${instance_dir}/jenkins.log &
-  
-  # Inform the user that Jenkins is starting
-  echo "Jenkins is being started from ${instance_dir}/jenkins.war on port ${port}. Logs can be found in ${instance_dir}/jenkins.log"
+  # Create systemd service file
+  sudo bash -c "cat > /etc/systemd/system/jenkins_${port}.service <<EOF
+[Unit]
+Description=Jenkins Continuous Integration Server on port ${port}
+Requires=network.target
+After=network.target
+
+[Service]
+Type=notify
+NotifyAccess=main
+User=$(whoami)
+Group=$(whoami)
+Environment=\"JENKINS_HOME=${instance_dir}\"
+ExecStart=/usr/bin/java -jar ${JENKINS_WAR_DIR}/jenkins.war --httpPort=${port} --prefix=\"/jenkins_${port}\"
+Restart=on-failure
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+  # Reload systemd to apply the new service file
+  sudo systemctl daemon-reload
+  sudo systemctl enable jenkins_${port}.service
+  sudo systemctl start jenkins_${port}.service
+
+  # Setup plugins for jenkins
 }
 
 # Check if at least one port number is provided
@@ -67,11 +92,3 @@ fi
 for port in "$@"; do
   setup_jenkins_instance ${port}
 done
-
-### Setup service file ###
-
-
-### Setup user privileges ###
-
-
-### Setup plugins for jenkins ###
